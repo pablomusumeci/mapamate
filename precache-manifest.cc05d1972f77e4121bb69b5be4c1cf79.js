72745ecc91c79410d8b776ca2d6fc53363fa1fa0
Pablo Musumeci <pmusumeci@plaid.com>
2024-05-22 16:25:02 +0200

    Updates

---
+++ precache-manifest.cc05d1972f77e4121bb69b5be4c1cf79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29378182736cae80be636bef922d774c",
+    "url": "/mapamate/index.html"
+  },
+  {
+    "revision": "401d0edcb810132e7bed",
+    "url": "/mapamate/static/css/main.77cb6375.chunk.css"
+  },
+  {
+    "revision": "3983614cfc5c1915638d",
+    "url": "/mapamate/static/js/2.ebd31059.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/mapamate/static/js/2.ebd31059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401d0edcb810132e7bed",
+    "url": "/mapamate/static/js/main.b6929f8a.chunk.js"
+  },
+  {
+    "revision": "ade249525a4562c90fea",
+    "url": "/mapamate/static/js/runtime-main.587364d5.js"
+  }
+]);
